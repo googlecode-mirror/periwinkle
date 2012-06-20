8cfdf084c279b0494524128fa4ba4989a41bfec2
george.stephen.french@gmail.com <george.stephen.french@gmail.com@5533c301-b736-96c6-3b3f-6578250d8b27>
2012-06-20 00:42:30 +0000

    update1

---
+++ trunk/HostilePlanet/src/Menu/Main.java
@@ -0,0 +1,37 @@
+package Menu;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Main extends StateBasedGame
+{
+	
+	public static final int MAINMENUSTATE = 0;
+	public static final int GAMEPLAYSTATE = 1;
+	
+	public Main()
+	{
+		super("test game");
+		
+		this.addState(new MainMenuState(MAINMENUSTATE));
+		//this.addState(new GameplayState(GAMEPLAYSTATE));
+		this.enterState(MAINMENUSTATE);
+	}
+
+	public static void main(String[] args) throws SlickException 
+	{
+		AppGameContainer game = new AppGameContainer(new Main());
+		
+		game.setDisplayMode(800, 600, false);
+		game.setTargetFrameRate(60);
+		game.start();
+	}
+
+	public void initStatesList(GameContainer gc) throws SlickException
+	{
+		this.getState(MAINMENUSTATE).init(gc, this);
+		//this.getState(GAMEPLAYSTATE).init(gc, this);
+	}
+}
--- trunk/phpsource/auth/google/return.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include_once("GoogleOpenID.php");
-
-  $googleLogin = GoogleOpenID::getResponse();
-  if($googleLogin->success()){
-    $user_id = $googleLogin->identity();
-  }
-?>
