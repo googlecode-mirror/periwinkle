7c27d8e9e5824bb21b2b2c24fbc2a2cfa558ea41
george.stephen.french <george.stephen.french@gmail.com>
2012-06-20 00:42:30 +0000

    update1

---
+++ HostilePlanet/src/Entity/RenderComponent.java
@@ -0,0 +1,16 @@
+package Entity;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public abstract class RenderComponent extends Component
+{
+	public RenderComponent(String id)
+    {
+		super.id = id;
+    }
+ 
+    public abstract void render(GameContainer gc, StateBasedGame sb, Graphics gr);
+}
--- phpsource/auth/google/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include_once("GoogleOpenID.php");
-  
-  $googleLogin = GoogleOpenID::createRequest("http://aoicms.org/cncms/phpsource/auth/return.php");
-  $googleLogin->redirect();
-?>
